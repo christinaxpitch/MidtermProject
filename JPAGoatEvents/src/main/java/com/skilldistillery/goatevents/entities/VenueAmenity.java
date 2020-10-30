075bc20ce96a36a7cf12658669ab54e19b476055
Ryan Miskoviak <ryan.j.miskoviak@gmail.com>
2020-10-30 13:33:34 -0600

    Added to Persistance.xml

---
+++ JPAGoatEvents/src/main/java/com/skilldistillery/goatevents/entities/VenueAmenity.java
@@ -0,0 +1,35 @@
+//package com.skilldistillery.goatevents.entities;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "venue_amenity")
+//public class VenueAmenity {
+//
+//	// FIELDS ============================================
+//	private Amenity amenity;
+//	private Venue venue;
+//
+//	// CONSTRUCTORS ======================================
+//	public VenueAmenity() {
+//		super();
+//	}
+//
+//	// GETTERS AND SETTERS =============================
+//	public Amenity getAmenity() {
+//		return amenity;
+//	}
+//
+//	public void setAmenity(Amenity amenity) {
+//		this.amenity = amenity;
+//	}
+//
+//	public Venue getVenue() {
+//		return venue;
+//	}
+//
+//	public void setVenue(Venue venue) {
+//		this.venue = venue;
+//	}
+//}
