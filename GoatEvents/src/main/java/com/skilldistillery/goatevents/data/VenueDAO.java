3031c811fe964d7eb1cddc16421336d37c9ea0a0
Christina <christina.pitch@gmail.com>
2020-11-01 18:01:34 -0700

    VENUE DAO done

--- GoatEvents/src/main/java/com/skilldistillery/goatevents/data/VenueDAO.java
+++ GoatEvents/src/main/java/com/skilldistillery/goatevents/data/VenueDAO.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.goatevents.data;
 
-public interface VenueDAO {
+import com.skilldistillery.goatevents.entities.Comment;
+import com.skilldistillery.goatevents.entities.Venue;
 
+public interface VenueDAO {
+	
+	public Venue addVenue(Venue venue);
+	public Venue updateVenue(int id, Venue Venue);
+	public boolean deleteVenue(int id);
+	
 }
--- GoatEvents/src/main/java/com/skilldistillery/goatevents/data/VenueDAOImpl.java
+++ GoatEvents/src/main/java/com/skilldistillery/goatevents/data/VenueDAOImpl.java
@@ -1,5 +1,51 @@
 package com.skilldistillery.goatevents.data;
 
-public class VenueDAOImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.goatevents.entities.Comment;
+import com.skilldistillery.goatevents.entities.Venue;
+
+@Service
+@Transactional
+public class VenueDAOImpl implements VenueDAO{
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Venue addVenue(Venue venue) {
+		em.persist(venue);
+		em.flush();
+		em.close();
+		return venue;
+	}
+
+	@Override
+	public Venue updateVenue(int id, Venue Venue) {
+		Venue newVenue = em.find(Venue.class, id);		
+		newVenue.setName(newVenue.getName());
+		newVenue.setAddress(newVenue.getAddress());
+		newVenue.setCapacity(newVenue.getCapacity());
+		newVenue.setDescription(newVenue.getDescription());
+		newVenue.setLogo(newVenue.getLogo());
+		newVenue.setCreatedAt(newVenue.getCreatedAt());
+		newVenue.setLastUpdate(newVenue.getLastUpdate());
+		return newVenue;
+	}
+
+	@Override
+	public boolean deleteVenue(int id) {
+		Venue deleteVenue = em.find(Venue.class, id);
+		em.remove(deleteVenue);	
+		boolean VenueWasDeleted = !em.contains(deleteVenue);
+		em.flush();
+		em.close();
+		return VenueWasDeleted;
+	}
 }
+
+
