cbad93302113d8a62188845ab1ba86c763259bee
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-11-02 17:14:54 -0700

    Search events and css set up

--- GoatEvents/src/main/java/com/skilldistillery/goatevents/controllers/SearchController.java
+++ GoatEvents/src/main/java/com/skilldistillery/goatevents/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.goatevents.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ public class SearchController {
 	
 	@RequestMapping (path = "search.do")
 	public String search(Model model, String keyword) {
+		keyword= "Doug";
 	User testuser = dao.getTestUser();
 		if(testuser.getImage() == null) {
 			testuser.setImage("https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg");
@@ -27,9 +29,23 @@ public class SearchController {
 		List<Event> eventList =  dao.findEventFromSearch(keyword);
 		List<Venue> venueList = dao.findVenueFromSearch(keyword);
 		int count = 0;
+		for (Event event : eventList) {
+			if(event.getImage() == null) {
+			event.setImage("https://thumbs.dreamstime.com/b/no-image-available-icon-vector-illustration-flat-design-140633878.jpg");
+			}
+			count++;
+		}
+		for (Venue venue : venueList) {
+			count++;
+			if(venue.getLogo() == null) {
+				venue.setLogo("https://thumbs.dreamstime.com/b/no-image-available-icon-vector-illustration-flat-design-140633878.jpg");
+			}
+		}
+			
 		model.addAttribute("count", count);
 		model.addAttribute("keyword", keyword);
 		model.addAttribute("events", eventList);
+		model.addAttribute("venues", venueList);
 		model.addAttribute("user", testuser);
 		return "searchresult";
 	}
